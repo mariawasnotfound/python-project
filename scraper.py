7756bbbf1a1f31e6bb8099dee416f27d56b6684b
root <root@MariaUbuntu22.04.3.myguest.virtualbox.org>
2024-12-21 18:56:30 +0500

    finished scrapper with coords!

--- scraper.py
+++ scraper.py
@@ -3,9 +3,22 @@ from bs4 import BeautifulSoup
 
 url = 'https://chel.guide/events'
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'}
-
 response = requests.get(url, headers=headers)
 soup = BeautifulSoup(response.content, 'html.parser')
+
+def get_coordinates(location):
+    location += ", Челябинск"
+    api_key = 'e3b1003031fa4e7cb32b832e08c2eda8'
+    url = f'https://api.opencagedata.com/geocode/v1/json?q={location}&key={api_key}'
+    response_ = requests.get(url)
+    data = response_.json()
+    results = data['results']
+
+    if results:
+        latitude = results[0]['geometry']['lat']
+        longitude = results[0]['geometry']['lng']
+        return latitude, longitude
+    return None, None
 
 events = []
 
@@ -18,13 +31,21 @@ for event in soup.find_all('div', class_='teaser_info'):
     time = details_parts[1].strip() if len(details_parts) > 1 else 'Время не указано'
     location = details_parts[2].strip() if len(details_parts) > 2 else 'Место не указано'
 
+    latitude, longitude = get_coordinates(location)
+
     events.append({
         'name': name,
         'date': date,
         'time': time,
-        'location': location
+        'location': location,
+        'lat': latitude,
+        'lng': longitude
     })
 
-for event in events:
-    print(f"Название: {event['name']}, Дата: {event['date']}, Время: {event['time']}, Место: {event['location']}")
 
+def save_events_to_db(events):
+    for event in events:
+        save_event(event['name'], event['date'], event['time'], event['location'], event['lat'], event['lng'])
+
+for event in events:
+	print(f"Название: {event['name']}, Дата: {event['date']}, Время: {event['time']}, Место: {event['location']}, Coords: {event['lat']}, {event['lng']}")
